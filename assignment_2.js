e43a89311c2fbf8aba2f168fd772572fbd2157c9
tongyonghang <57294615+tongyonghang@users.noreply.github.com>
2021-01-11 13:37:50 +0800

    Add files via upload
    Final Refined version without having rotation settings seperately for each obj, all the changes syn to 3 object at the same time.

--- assignment_2.js
+++ assignment_2.js
@@ -1,33 +1,20 @@
 var canvas;
 var gl;
 
-var numVertices  = 36;
+var NumVertices  = 36; //cube
+var NumVertices_1  = 12; //pyramid
 
 var pointsArray = [];
 var normalsArray = [];
 
-var vertices = [
-        vec4( -0.5, -0.5,  0.5, 1.0 ),
-        vec4( -0.5,  0.5,  0.5, 1.0 ),
-        vec4( 0.5,  0.5,  0.5, 1.0 ),
-        vec4( 0.5, -0.5,  0.5, 1.0 ),
-        vec4( -0.5, -0.5, -0.5, 1.0 ),
-        vec4( -0.5,  0.5, -0.5, 1.0 ),
-        vec4( 0.5,  0.5, -0.5, 1.0 ),
-        vec4( 0.5, -0.5, -0.5, 1.0 ),
+var projectionMatrix;
+var modelViewMatrixLoc;
+var projectionMatrixLoc;
 
-        //sphere
-	      vec4(0.0, 0.0, -1.0,1),
-	      vec4(0.0, 0.942809, 0.333333, 1),
-	      vec4(-0.816497, -0.471405, 0.333333, 1),
-         vec4(0.816497, -0.471405, 0.333333,1),
-         
-         //pyramind
-         vec4( 0.0, -0.50, -1.00),
-         vec4( 0.0,  0.50,  0.00),
-         vec4( 1.0, -0.50,  0.50),
-         vec4(-1.0, -0.50,  0.50)
-    ];
+var modelView; // cube
+var modelViewMatrix; //sphere
+var modelViewMatrix_1; //pyramid
+
 
 var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
 var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
@@ -41,7 +28,7 @@ var materialShininess = 100.0;
 
 var ctm;
 var ambientColor, diffuseColor, specularColor;
-var modelView, projection;
+
 var viewerPos;
 var program;
 
@@ -49,37 +36,53 @@ var xAxis = 0;
 var yAxis = 1;
 var zAxis = 2;
 var axis = 0;
-var theta =[45.0, 45.0, 45.0]; // to set the initial rotation of object
+var theta =[45.0, 0.0, 0.0]; // to set the initial rotation of object
 
-var speed =3.0;
-
+var speed =3.0; // rotation speed
 var flag = true;
 
 //sphere attribute
 var numTimesToSubdivide = 4;
 var index = 0;
-var modelViewMatrix;
-var projectionMatrix;
-var modelViewMatrixLoc;
-var projectionMatrixLoc;
-var at = vec3(0.0, 0.0, 0.0);
-var up = vec3(0.0, 1.0, 0.0);
-var left = -5.0;
-var right = 5.0;
-var ytop = 5.5;
-var bottom = -5.5;
+
+
+// camera position
+var left = -4.0;
+var right = 4.0;
+var ytop = 3.0;
+var bottom = -3.0;
 var near = -20;
-var far = 20;
+var far = 30;
 
 
-var NumVertices1  = 12;
-var modelViewMatrix_1;
+var vertices = [
+   vec4( -0.5, -0.5,  0.5, 1.0 ),
+   vec4( -0.5,  0.5,  0.5, 1.0 ),
+   vec4( 0.5,  0.5,  0.5, 1.0 ),
+   vec4( 0.5, -0.5,  0.5, 1.0 ),
+   vec4( -0.5, -0.5, -0.5, 1.0 ),
+   vec4( -0.5,  0.5, -0.5, 1.0 ),
+   vec4( 0.5,  0.5, -0.5, 1.0 ),
+   vec4( 0.5, -0.5, -0.5, 1.0 ),
+
+   //sphere
+    vec4(0.0, 0.0, -1.0,1),
+    vec4(0.0, 0.942809, 0.333333, 1),
+    vec4(-0.816497, -0.471405, 0.333333, 1),
+    vec4(0.816497, -0.471405, 0.333333,1),
+    
+    //pyramind
+    vec4( 0.0, -0.50, -1.00),
+    vec4( 0.0,  0.50,  0.00),
+    vec4( 1.0, -0.50,  0.50),
+    vec4(-1.0, -0.50,  0.50)
+];
 function colorPyramid()
 {
-    quad1( 12, 13, 14 ); // bottom
-    quad1( 12, 14, 15 ); // side0
-    quad1( 14, 12, 15 ); // side1
-    quad1( 15, 13, 12 ); // side3
+    quad1( 12, 13, 14 ); 
+    quad1( 12, 14, 15 ); 
+    quad1( 14, 12, 15 ); 
+    quad1( 15, 13, 12 );
 }
 
 function quad1(a, b, c) 
@@ -187,12 +190,12 @@ window.onload = function init() {
     ambientProduct = mult(lightAmbient, materialAmbient);
     diffuseProduct = mult(lightDiffuse, materialDiffuse);
     specularProduct = mult(lightSpecular, materialSpecular);
-
-    // pyramid attribute
-    colorPyramid();
    
     //cube attrivute
     colorCube();
+
+    // pyramid attribute
+    colorPyramid();
 
     //sphere attribute
     tetrahedron(vertices[8], vertices[9], vertices[10], vertices[11], numTimesToSubdivide);
@@ -216,14 +219,28 @@ window.onload = function init() {
     modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
     projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
 
+    //Function to change rotation
     document.getElementById("x_axis").onclick = function(){axis = xAxis;};
     document.getElementById("y_axis").onclick = function(){axis = yAxis;};
     document.getElementById("z_axis").onclick = function(){axis = zAxis;};
     document.getElementById("toggle").onclick = function(){flag = !flag;};
-
-   
+    
+    //Function to get object speed
     document.getElementById("Speed_2").onchange = function(){
         speed = parseInt(document.getElementById("Speed_1").value);
+    };
+
+    //Function to reset settings
+    document.getElementById("Reset").onclick = function(){
+      theta =[45.0, 0.0, 0.0];
+      axis = xAxis;
+      speed = 3.0;
+      flag = true;
+      document.getElementById("Speed_1").value = speed;
+      document.getElementById("Speed_2").value = speed;
+      
+      
+
     };
 
     gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"),
@@ -243,45 +260,46 @@ window.onload = function init() {
 
 var render = function(){
             
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-            
-    if(flag) theta[axis] += speed; //speed of the object
+   gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+           
+   if(flag) theta[axis] += speed; //speed of the object
 
-    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
-    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+   projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+   gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
 
-    modelView = mat4();
-    modelView = mult(modelView, rotate(theta[xAxis], [1, 0, 0] ));
-    modelView = mult(modelView, rotate(theta[yAxis], [0, 1, 0] ));
-    modelView = mult(modelView, rotate(theta[zAxis], [0, 0, 1] ));
+   //create cube 
+   modelView = mat4();
+   modelView = mult(modelView, rotate(theta[xAxis], [1, 0, 0] ));
+   modelView = mult(modelView, rotate(theta[yAxis], [0, 1, 0] ));
+   modelView = mult(modelView, rotate(theta[zAxis], [0, 0, 1] ));
+   modelView = mult(modelView, translate(-2.8,0,0));
 
-    modelView = mult(modelView, translate(3.5,0,0));
+   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelView));
 
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelView));
+   gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+  
+   //create sphere
+   modelViewMatrix = mat4();
+   modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis], [1, 0, 0]));
+   modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis], [0, 1, 0]));
+   modelViewMatrix = mult(modelViewMatrix, rotate(theta[zAxis], [0, 0, 1]));
 
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices+NumVertices1 );
+   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+
+   for( var i=NumVertices+NumVertices_1; i<index+NumVertices+NumVertices_1; i+=3) 
+       gl.drawArrays( gl.TRIANGLES, i, 3 );
+
+
+   //create pyramid
+   modelViewMatrix_1 = mat4();
+   modelViewMatrix_1 = mult(modelViewMatrix_1, rotate(theta[xAxis], [1, 0, 0]));
+   modelViewMatrix_1 = mult(modelViewMatrix_1, rotate(theta[yAxis], [0, 1, 0]));
+   modelViewMatrix_1 = mult(modelViewMatrix_1, rotate(theta[zAxis], [0, 0, 1]));
+   modelViewMatrix_1 = mult(modelViewMatrix_1, translate(2.8,0,0));
    
-    modelViewMatrix = mat4();
-    modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis], [1, 0, 0]));
-    modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis], [0, 1, 0]));
-    modelViewMatrix = mult(modelViewMatrix, rotate(theta[zAxis], [0, 0, 1]));
+   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix_1) );
 
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-        
-    for( var i=numVertices+NumVertices1; i<index+numVertices+NumVertices1; i+=3) 
-        gl.drawArrays( gl.TRIANGLES, i, 3 );
+   gl.drawArrays( gl.TRIANGLES, NumVertices, NumVertices_1);
 
-    modelViewMatrix_1 = mat4();
-    modelViewMatrix_1 = mult(modelViewMatrix_1, rotate(theta[xAxis], [1, 0, 0]));
-    modelViewMatrix_1 = mult(modelViewMatrix_1, rotate(theta[yAxis], [0, 1, 0]));
-    modelViewMatrix_1 = mult(modelViewMatrix_1, rotate(theta[zAxis], [0, 0, 1]));
-
-    modelViewMatrix_1 = mult(modelViewMatrix_1, translate(-3.5,0,0));
-
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix_1) );
-
-    gl.drawArrays( gl.TRIANGLES, 0, NumVertices1 );
-
-    requestAnimFrame( render );       
-    
+   requestAnimFrame( render );       
 }
